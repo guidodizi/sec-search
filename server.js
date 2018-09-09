8f12244f05c470a2c95a68bbf0a2a5908c8d34ba
Guido Dizioli <guidodizi@gmail.com>
2018-09-08 21:07:45 -0300

    deploy for heroku

--- server.js
+++ server.js
@@ -1,43 +1,15 @@
+//Install express server
 const express = require("express");
-const request = require("request");
-const cors = require("cors");
-var parseString = require("xml2js").parseString;
+const path = require("path");
 
 const app = express();
-app.use(cors());
 
-app.get("/:company/:page?", (req, res) => {
-  const page = req.params.page ? req.params.page : 0;
-  request(
-    `https://www.sec.gov/cgi-bin/browse-edgar?CIK=${
-      req.params.company
-    }&owner=exclude&action=getcompany&output=xml&start=${page * 40}&count=${(page + 1) * 40}`,
-    { headers: { Accept: "application/xml" } },
-    (err, resp, body) => {
-      if (err) {
-        console.log(err);
-      }
-      parseString(body, (err, result) => {
-        if (err) {
-          console.log(err);
-        }
-        if (result) {
-          res.json({
-            result: {
-              name: result.companyFilings.companyInfo[0].name,
-              fillings: result.companyFilings.results[0].filing
-            },
-            errors: []
-          });
-        } else {
-          res.json({
-            result: {},
-            errors: ["There was error while retrieving the company's data"]
-          });
-        }
-      });
-    }
-  );
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/sec-search"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/sec-search/index.html"));
 });
 
-app.listen(3000, () => console.log("Listening on 3000"));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
